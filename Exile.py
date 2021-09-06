68833fe86335722cb2d5aed04dd1c71ef3842803
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-09-06 15:01:48 +0300

    Create Exile.py

---
+++ Exile.py
@@ -0,0 +1,17 @@
+import time
+
+
+t1 = time.time()
+x = "Panx Romana state closed" 
+y = "The Sound Missiles"
+z = "Diary of Dreams Exile"
+w = "Black Sabbath Orchid"
+
+print(bool(x))
+print(bool(y))
+print(bool(z))
+print(bool(w))
+
+t2 = time.time()
+t = t2 - t1
+print("Elapsed time is : ", t, " seconds")
