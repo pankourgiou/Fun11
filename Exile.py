1fc7279a4cc775597e279cfcad6598b72d2ad6ba
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-09-08 07:41:16 +0300

    Update Exile.py

--- Exile.py
+++ Exile.py
@@ -7,12 +7,16 @@ y = "The Sound Missiles"
 z = "Diary of Dreams Exile"
 w = "Black Sabbath Orchid"
 a = "Myles Kennedy a Thousand words"
+b = "Midnight suns Iknow when the sun rises I know when the sun sets"
+c = "Myles Kennedy In the year of the tiger"
 
 print(bool(x))
 print(bool(y))
 print(bool(z))
 print(bool(w))
 print("Perfect take-true words")
+print("cool")
+print("cool")
 
 t2 = time.time()
 t = t2 - t1
