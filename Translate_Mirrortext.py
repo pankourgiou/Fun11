d90c5d4db6084b4448b96189d1d740fd2844e5e3
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2022-12-31 03:52:40 +0200

    Update Translate_Mirrortext.py

--- Translate_Mirrortext.py
+++ Translate_Mirrortext.py
@@ -1,4 +1,31 @@
 #Install translators module
+#The sounds of the letters from A-Z are:
+#buh
+#dah
+#guh
+#zuh
+#meh
+#tuh
+#luh
+#suh
+#fuh
+#nuh
+#kuh
+#puh
+#rah
+#vuh
+#yuh
+#chuh
+#juh
+#wuh
+#shuh
+#xuh
+#thuh
+#muh
+#quh
+#gah
+#duh
+#beh
 import translators as ts
 
 text = 'cheese'
